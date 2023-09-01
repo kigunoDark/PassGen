7a9967c348bd87c315b0087c75958cd2158541a7
kigunoDark <kiguno1996@gmail.com>
2023-09-01 21:08:14 +0300

    more eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,40 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: "^18.2.0",
     },
-    "extends": "plugin:react/recommended",
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+  },
+  extends: "plugin:react/recommended",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
     },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        'indent': ['error', 2], // Enforce 2-space indentation
-        'semi': ['error', 'always'], // Require semicolons at the end of statements
-        'no-var': 'error', // Use let or const instead of var
-        'prefer-const': 'error', // Suggest using const for non-reassigned variables
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    indent: ["error", 2], // Enforce 2-space indentation
+    semi: ["error", "always"], // Require semicolons at the end of statements
+    "no-var": "error", // Use let or const instead of var
+    "prefer-const": "error", // Suggest using const for non-reassigned variables
 
-        // React-specific rules
-        'react/jsx-uses-react': 'error', // Prevent React from being marked as unused
-        'react/jsx-uses-vars': 'error', // Prevent variables used in JSX from being marked as unused
-        'react/react-in-jsx-scope': 'error', // Prevent React from being incorrectly marked as unused
-        'react/prop-types': 'warn', // Warn if PropTyp
-    }
-}
+    // React-specific rules
+    "react/jsx-uses-react": "error", // Prevent React from being marked as unused
+    "react/jsx-uses-vars": "error", // Prevent variables used in JSX from being marked as unused
+    "react/react-in-jsx-scope": "error", // Prevent React from being incorrectly marked as unused
+    "react/prop-types": "warn", // Warn if PropTyp
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,24 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-        ],
-    },
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
